5ec4da981606b666b86cb64a4ef5814784ac0124
BK-TN <me@bk-tn.com>
2016-04-06 23:56:09 +0200

    Improved (and commented) the drawer class

--- Library/Drawer.cs
+++ Library/Drawer.cs
@@ -7,46 +7,62 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Battle_Mages
 {
+    /// <summary>
+    /// Represents a layer that can be drawn to. Layers are ordered by their constant value.
+    /// </summary>
     internal enum DrawLayer
     {
-        Background = -1,
-        Gameplay = 0,
-        Foreground = 1,
-        UI = 2,
-        AboveUI = 3
+        //The layers will be ordered by the order they are declared in here
+        Background,
+        Gameplay,
+        Foreground,
+        UI,
+        AboveUI
     }
 
     /// <summary>
-    /// A "drawer" of SpriteBatches to "draw" to
+    /// A "drawer" of SpriteBatches to "draw" to.
     /// </summary>
     internal class Drawer
     {
-        private Dictionary<DrawLayer, SpriteBatch> layers = new Dictionary<DrawLayer, SpriteBatch>();
+        private Dictionary<DrawLayer, SpriteBatch> layers;
         private GraphicsDevice graphics;
-        private Matrix matrix;
+        public Matrix Matrix { get; set; } = Matrix.Identity;
 
-        public Drawer(GraphicsDevice graphics, Matrix matrix)
+        public Drawer(GraphicsDevice graphics)
         {
             this.graphics = graphics;
-            this.matrix = matrix;
+
+            //Create a dictionary to hold layers and add all values of the DrawLayer enum
+            layers = new Dictionary<DrawLayer, SpriteBatch>();
+            foreach (DrawLayer layerId in Enum.GetValues(typeof(DrawLayer)))
+                layers.Add(layerId, new SpriteBatch(graphics));
         }
 
+        /// <summary>
+        /// Gets the SpriteBatch associated with a layer. Use this to draw on the batch.
+        /// </summary>
+        /// <param name="layer"></param>
+        /// <returns></returns>
         public SpriteBatch this[DrawLayer layer]
         {
             get
             {
-                if (!layers.ContainsKey(layer))
-                {
-                    SpriteBatch batch = new SpriteBatch(graphics);
-                    batch.Begin(SpriteSortMode.Deferred, transformMatrix: matrix);
-                    layers.Add(layer, new SpriteBatch(graphics));
-                }
                 return layers[layer];
             }
         }
 
-        public void End()
+        public void BeginBatches()
         {
+            foreach (SpriteBatch batch in layers.Values)
+            {
+                batch.Begin(SpriteSortMode.Deferred, transformMatrix: Matrix);
+            }
+        }
+
+        public void EndBatches()
+        {
+            //End all layers in a loop so they are drawn in order
             foreach (SpriteBatch batch in layers.Values)
             {
                 batch.End();
