3d6c9582497a42c726de32779fba1f127ff371d1
BK-TN <me@bk-tn.com>
2016-05-11 15:49:53 +0200

    fireball spell that does nothing

---
+++ Spells/Fireball.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Battle_Mages
+{
+    internal class Fireball : Spell
+    {
+        public Fireball(Rune[] runes) : base(runes)
+        {
+            Damage = 120120301;
+        }
+    }
+}
