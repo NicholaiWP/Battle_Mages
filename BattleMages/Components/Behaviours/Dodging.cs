61712d5234581e255011ac13bff8ae112d0ac21f
Mathias Larsen <mathiasdodo@hotmail.com>
2016-05-24 13:42:08 +0200

    - Added dodge class

---
+++ BattleMages/Components/Behaviours/Dodging.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleMages
+{
+    public class Dodging : IBehaviour
+    {
+        public void ExecuteBehaviour(float attackRange, float targetingRange)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
