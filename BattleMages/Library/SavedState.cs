e7a8371b13aae43d638300fbf64464c665eaef5a
Mathias Larsen <mathiasdodo@hotmail.com>
2016-05-30 10:14:02 +0200

    - some database

--- BattleMages/Library/SavedState.cs
+++ BattleMages/Library/SavedState.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -15,53 +16,40 @@ namespace BattleMages
         private List<SpellInfo> spellBook = new List<SpellInfo>();
         private List<int> spellBar = new List<int>();
         private SQLiteConnection connection = new SQLiteConnection("Data Source = BMdatabase.db; Version = 3;");
-
+        private string databaseFileName = "BMdatabase.db";
         public List<SpellInfo> SpellBook { get { return spellBook; } }
         public List<int> SpellBar { get { return spellBar; } }
 
+        public void CreateDatabaseFile()
+        {
+            if (!File.Exists(databaseFileName))
+            {
+                SQLiteConnection.CreateFile(databaseFileName);
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand("create table IF NOT EXISTS SpellBook(Id integer primary key, spellId int)",
+                    connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (SQLiteCommand command = new SQLiteCommand("create table IF NOT EXISTS Runes(Id integer primary key, RuneId integer, SpellBookId integer REFERENCES SpellBook(Id))",
+                    connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (SQLiteCommand command = new SQLiteCommand("create table IF NOT EXISTS SpellBar(Id integer primary key, SpellBookId integer REFERENCES SpellBook(Id))",
+                    connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
         public void Save()
         {
-            SQLiteConnection.CreateFile("BMdatabase.db");
-            CreateTables();
-            InsertToTables();
-        }
-
-        private void CreateTables()
-        {
-            connection.Open();
-
-            using (SQLiteCommand command = new SQLiteCommand("create table IF NOT EXISTS SpellBook(id integer primary key, spellId int, rune1Id int, rune2Id int, rune3Id int, rune4Id int)",
-                connection))
-            {
-                command.ExecuteNonQuery();
-            }
-
-            using (SQLiteCommand command = new SQLiteCommand("create table IF NOT EXISTS SpellBar(spellId int, rune1Id int, rune2Id int, rune3Id int, rune4Id int)",
-                connection))
-            {
-                command.ExecuteNonQuery();
-            }
-
-            using (SQLiteCommand command = new SQLiteCommand("create table IF NOT EXISTS ChallengesCompleted(challengeId int)",
-                connection))
-            {
-                command.ExecuteNonQuery();
-            }
-            connection.Close();
-        }
-
-        private void InsertToTables()
-        {
-            connection.Open();
-            using (SQLiteCommand command = new SQLiteCommand("DELETE * from SpellBook", connection))
-            {
-                command.ExecuteNonQuery();
-            }
-
-            using (SQLiteCommand command = new SQLiteCommand(@"INSERT INTO SpellBook VALUES(null, @spellId, @rune1Id, @rune2Id, @rune3Id, @rune4Id)",
-                connection))
-            {
-            }
         }
     }
 }
