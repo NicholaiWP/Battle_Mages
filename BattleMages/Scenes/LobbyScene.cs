d7e528c697b9afd1d814b45d3822bdd45c2bda54
Mathias Larsen <mathiasdodo@hotmail.com>
2016-05-31 12:40:26 +0200

    - allow movement camera true

--- BattleMages/Scenes/LobbyScene.cs
+++ BattleMages/Scenes/LobbyScene.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace BattleMages
 {
