19e9df7b6c59c3ea0311727cd6f435229dc65964
Mathias Larsen <mathiasdodo@hotmail.com>
2016-06-01 23:08:43 +0200

    forgot to push commit

--- BattleMages/Scenes/ShopScene.cs
+++ BattleMages/Scenes/ShopScene.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace BattleMages
 {
@@ -20,34 +20,32 @@ namespace BattleMages
         private Texture2D rune3;
         private string bottomLeftText = string.Empty;
         private string upperRightText = string.Empty;
-        private Dictionary<string, Vector2> descriptionPlacement = new Dictionary<string, Vector2>();
         private int runeCost;
         private int StartingMoney;
         private int currentMoney;
+        private int buttonSelected;
 
         //bool for returning to the lobby after entering the shop
         private bool cPressed;
 
         public int Cost { get { return runeCost; } set { runeCost = value; } }
 
-        public int StartingMoney1 {get {return StartingMoney;} set {StartingMoney = value;}}
+        public int StartingMoney1 { get { return StartingMoney; } set { StartingMoney = value; } }
 
-        public int CurrentMoney { get {return currentMoney;} set {currentMoney = value;}}
+        public int CurrentMoney { get { return currentMoney; } set { currentMoney = value; } }
 
         //TO DO:
-        //-check whether the rune/button is selected
         //-then subtracts currentMoney from the rune's cost and adds the rune to the spellbook's rune slots.
         //-set the rune's Cost and write currentMoney into the game using perhaps a drawstring, convert the int's number to a string in this AboveUI draw sentence.
         //-On coin/money pick-up, add money to currentMoney counter.
 
         public ShopScene(Scene oldScene)
         {
-            
             var content = GameWorld.Instance.Content;
             this.oldScene = oldScene;
             StartingMoney1 = 50;
             CurrentMoney -= Cost;
-
+            buttonSelected = -1;
             background = content.Load<Texture2D>("Backgrounds/Shop");
             font = content.Load<SpriteFont>("FontBM");
             titleFont = content.Load<SpriteFont>("TitleFont");
@@ -59,27 +57,25 @@ namespace BattleMages
 
             int xPos = 0;
             int yPos = 0;
-
             foreach (AttributeRune attrRune in StaticData.AttributeRunes)
             {
-              
-                    var Button_Rune = content.Load<Texture2D>("Images/Button_Rune");
-                    var Button_Rune_Hover = content.Load<Texture2D>("Images/Button_Rune_Hover");
-                    AddObject(ObjectBuilder.BuildButton(new Vector2(GameWorld.Camera.Position.X - GameWorld.GameWidth / 2 + 16 + xPos * 16, GameWorld.Camera.Position.Y - GameWorld.GameHeight / 2 + 15 + yPos * 16), Button_Rune, Button_Rune_Hover,
-                        () =>
-                        {
-                            bottomLeftText = attrRune.Description;
-                            upperRightText = attrRune.RuneCost.ToString();
-                        },
-                        null, false));
-
-                    xPos++;
-                    if (xPos >= 2)
+                var Button_Rune = content.Load<Texture2D>("Images/Button_Rune");
+                var Button_Rune_Hover = content.Load<Texture2D>("Images/Button_Rune_Hover");
+                AddObject(ObjectBuilder.BuildButton(new Vector2(GameWorld.Camera.Position.X - GameWorld.GameWidth / 2 + 16 + xPos * 16, GameWorld.Camera.Position.Y - GameWorld.GameHeight / 2 + 15 + yPos * 16), Button_Rune, Button_Rune_Hover,
+                    () =>
                     {
-                        yPos++;
-                        xPos = 0;
-                  
-                    }
+                        buttonSelected = StaticData.AttributeRunes.IndexOf(attrRune);
+                        bottomLeftText = attrRune.Description;
+                        upperRightText = attrRune.RuneCost.ToString();
+                    },
+                    null, false));
+
+                xPos++;
+                if (xPos >= 2)
+                {
+                    yPos++;
+                    xPos = 0;
+                }
 
                 if (Cost > currentMoney)
                 {
@@ -95,20 +91,14 @@ namespace BattleMages
                     AddObject(ObjectBuilder.BuildButton(new Vector2(GameWorld.Camera.Position.X - GameWorld.GameWidth / 2 + 150, GameWorld.Camera.Position.Y - GameWorld.GameHeight / 2 + 80), shopButton, shopButton_hover,
                      () =>
                      {
-                         if(selectedRune != SelectedRune.None)
+                         if (buttonSelected == 0)
                          {
-
                          }
                      },
                      null, false));
                 }
-                  
-        }   
-
-      
-          
-     }
-        
+            }
+        }
 
         public override void Draw(Drawer drawer)
         {
@@ -140,6 +130,7 @@ namespace BattleMages
         public override void Update()
         {
             base.Update();
+
             KeyboardState state = Keyboard.GetState();
             if (state.IsKeyDown(Keys.C))
             {
