9ecb64ec2f40ccc878121316428f62eb97a0cc91
BK-TN <me@bk-tn.com>
2016-06-02 11:00:50 +0200

    IngameUI now uses SpellInfoRenderers

--- BattleMages/Components/SpellInfoRenderer.cs
+++ BattleMages/Components/SpellInfoRenderer.cs
@@ -24,24 +24,30 @@ namespace BattleMages
         private void Draw(DrawMsg msg)
         {
             Vector2 pos = GameObject.Transform.Position;
-            msg.Drawer[DrawLayer.UI].Draw(background, position: pos - Utils.HalfTexSize(background));
 
-            if (spellInfo == null) return;
-
-            BaseRune baseRune = spellInfo.GetBaseRune();
-            if (baseRune != null)
+            if (spellInfo == null)
             {
-                Texture2D baseRuneTex = spellInfo.GetBaseRune().Texture;
-                msg.Drawer[DrawLayer.UI].Draw(baseRuneTex, position: pos - Utils.HalfTexSize(baseRuneTex));
+                msg.Drawer[DrawLayer.UI].Draw(background, position: pos - Utils.HalfTexSize(background), color: Color.FromNonPremultiplied(255, 255, 255, 128));
             }
-
-            Vector2[] runePositions = new Vector2[] { new Vector2(0, -6), new Vector2(6, 0), new Vector2(0, 6), new Vector2(-6, 0) };
-            for (int j = 0; j < SpellInfo.AttributeRuneSlotCount; j++)
+            else
             {
-                AttributeRune attrRune = spellInfo.GetAttributeRune(j);
-                if (attrRune != null)
+                msg.Drawer[DrawLayer.UI].Draw(background, position: pos - Utils.HalfTexSize(background));
+
+                BaseRune baseRune = spellInfo.GetBaseRune();
+                if (baseRune != null)
                 {
-                    msg.Drawer[DrawLayer.UI].Draw(attrRune.Texture, position: pos + runePositions[j] - Utils.HalfTexSize(attrRune.Texture));
+                    Texture2D baseRuneTex = spellInfo.GetBaseRune().Texture;
+                    msg.Drawer[DrawLayer.UI].Draw(baseRuneTex, position: pos - Utils.HalfTexSize(baseRuneTex));
+                }
+
+                Vector2[] runePositions = new Vector2[] { new Vector2(0, -6), new Vector2(6, 0), new Vector2(0, 6), new Vector2(-6, 0) };
+                for (int j = 0; j < SpellInfo.AttributeRuneSlotCount; j++)
+                {
+                    AttributeRune attrRune = spellInfo.GetAttributeRune(j);
+                    if (attrRune != null)
+                    {
+                        msg.Drawer[DrawLayer.UI].Draw(attrRune.Texture, position: pos + runePositions[j] - Utils.HalfTexSize(attrRune.Texture));
+                    }
                 }
             }
         }
