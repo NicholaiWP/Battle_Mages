64ab9ae86f3313c15a9410ca27c9918597c25191
Mathias Larsen <mathiasdodo@hotmail.com>
2016-06-04 16:41:27 +0200

    playing enemy death animation

--- BattleMages/Components/Enemy.cs
+++ BattleMages/Components/Enemy.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace BattleMages
 {
@@ -68,8 +68,6 @@ namespace BattleMages
 
                 if (health <= 0)
                 {
-                    //Removes enemy object
-                    GameWorld.Scene.RemoveObject(GameObject);
                     //Spawns a number of coins when enemy dies
                     for (int i = 0; i < moneyAmount; i++)
                     {
