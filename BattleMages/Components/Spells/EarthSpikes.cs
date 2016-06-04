337af50bf333aad7f38867cae349bafc382955ab
BK-TN <me@bk-tn.com>
2016-06-05 00:30:08 +0200

    Loads of spellbook fixes and tweaks

--- BattleMages/Components/Spells/EarthSpikes.cs
+++ BattleMages/Components/Spells/EarthSpikes.cs
@@ -43,7 +43,7 @@ namespace BattleMages
             animator.CreateAnimation("Down", new Animation(0, 8, 0, 8, 32, 32, 20, Vector2.Zero));
             GameObject.AddComponent(animator);
 
-            collider = new Collider(new Vector2(spriteRenderer.Sprite.Width, spriteRenderer.Sprite.Height));
+            collider = new Collider(new Vector2(48, 48));
             GameObject.AddComponent(collider);
         }
 
