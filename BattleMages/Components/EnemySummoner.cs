371b0233f15344bca228b6f24bf1ec8c452fabd9
BK-TN <me@bk-tn.com>
2016-06-06 11:46:16 +0200

    Enemy summon animation implemented

---
+++ BattleMages/Components/EnemySummoner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleMages
+{
+    internal class EnemySummoner : Component
+    {
+        private Animator animator;
+        private Action onSummon;
+
+        public EnemySummoner(Action onSummon)
+        {
+            this.onSummon = onSummon;
+
+            Listen<InitializeMsg>(Initialize);
+            Listen<UpdateMsg>(Update);
+            Listen<AnimationDoneMsg>(AnimationDone);
+        }
+
+        private void Initialize(InitializeMsg msg)
+        {
+            animator = GameObject.GetComponent<Animator>();
+        }
+
+        private void Update(UpdateMsg msg)
+        {
+            animator.PlayAnimation("");
+
+            if (animator.CurrentIndex >= 16)
+            {
+                onSummon?.Invoke();
+                onSummon = null;
+            }
+        }
+
+        private void AnimationDone(AnimationDoneMsg msg)
+        {
+            GameWorld.Scene.RemoveObject(GameObject);
+        }
+    }
+}
