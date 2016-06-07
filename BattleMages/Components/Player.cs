720b91271f2305f520fdf7c0bd158863fa59f3d8
BK-TN <me@bk-tn.com>
2016-06-07 09:51:24 +0200

    Final dialouge poop

--- BattleMages/Components/Player.cs
+++ BattleMages/Components/Player.cs
@@ -185,7 +185,8 @@ namespace BattleMages
                 {
                     //Create spell object and add it to the world
                     GameObject spellObject = new GameObject(transform.Position);
-                    Spell spellComponent = baseRune.CreateSpell(new SpellCreationParams(spellToCast, targetPoint, character.Velocity));
+                    Spell spellComponent = baseRune.CreateSpell(
+                        new SpellCreationParams(spellToCast, targetPoint, character.Velocity));
                     spellObject.AddComponent(spellComponent);
                     GameWorld.Scene.AddObject(spellObject);
 
